ca3907bd9328202587b7f75b33c747bf0ffd5f07
Nicholas Delkeskamp <ndelkeskamp@gmail.com>
2018-07-28 22:00:11 +0200

    add stuff

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,52 +1,70 @@
-import React, {Component} from 'react';
-import {StyleSheet, View} from 'react-native';
-import {RichTextEditor} from 'react-native-zss-rich-text-editor';
-import WebViewBridge from 'react-native-webview-bridge';
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Platform
+} from 'react-native';
+import {RichTextEditor, RichTextToolbar} from 'react-native-zss-rich-text-editor';
+import KeyboardSpacer from 'react-native-keyboard-spacer';
 
+export default class App extends Component {
 
-type Props = {};
-export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.getHTML = this.getHTML.bind(this);
+    this.setFocusHandlers = this.setFocusHandlers.bind(this);
+  }
+
   render() {
     return (
-      <View>
-        <RichTextEditor
-        ref={(r) => this.richtext = r}
-        initialTitleHTML={'Title!!'}
-        initialContentHTML={'Hello <b>World</b> <p>this is a new paragraph</p> <p>this is another new paragraph</p>'}
-        editorInitializedCallback={() => this.onEditorInitialized()}
-        />
-      </View>
+        <View style={styles.container}>
+          <RichTextEditor
+              ref={(r)=>this.richtext = r}
+              style={styles.richText}
+              initialTitleHTML={'Title!!'}
+              initialContentHTML={'Hello <b>World</b> <p>this is a new paragraph</p> <p>this is another new paragraph</p>'}
+              editorInitializedCallback={() => this.onEditorInitialized()}
+          />
+          <RichTextToolbar
+            getEditor={() => this.richtext}
+          />
+          {Platform.OS === 'ios' && <KeyboardSpacer/>}
+        </View>
     );
   }
+
+  onEditorInitialized() {
+    this.setFocusHandlers();
+    this.getHTML();
+  }
+
+  async getHTML() {
+    const titleHtml = await this.richtext.getTitleHtml();
+    const contentHtml = await this.richtext.getContentHtml();
+    //alert(titleHtml + ' ' + contentHtml)
+  }
+
+  setFocusHandlers() {
+    this.richtext.setTitleFocusHandler(() => {
+      //alert('title focus');
+    });
+    this.richtext.setContentFocusHandler(() => {
+      //alert('content focus');
+    });
+  }
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    backgroundColor: 'white',
+    flexDirection: 'column',
+    backgroundColor: '#ffffff',
+    paddingTop: 40
   },
-  header: {
-    fontWeight: 'bold',
-    fontSize: 30,
-    textAlign: 'center',
-    marginTop: 20,
-  },
-  description: {
-    textAlign: 'center',
-    color: '#333333',
-    fontStyle: 'italic',
-  },
-  cta: {
-    textAlign: 'center',
-    fontWeight: 'bold',
-    color: 'red',
-    marginTop: 20,
-    marginBottom: 20,
-  },
-  darkGreyButton: {
-    color: "#DFDFDF",
+  richText: {
+    alignItems:'center',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
   },
 });
